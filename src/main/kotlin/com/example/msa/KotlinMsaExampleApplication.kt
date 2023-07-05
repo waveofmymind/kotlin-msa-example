b234da9f70a92a50d54f5234386863982481410c
waveofmymind <sjun0913@gmail.com>
2023-07-06 01:45:35 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/msa/KotlinMsaExampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.msa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinMsaExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinMsaExampleApplication>(*args)
+}
