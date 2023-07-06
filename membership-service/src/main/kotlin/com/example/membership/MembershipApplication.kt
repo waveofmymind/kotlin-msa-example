1121e5adcf707ad3dd9b7e3fc5759b67b6093da2
waveofmymind <sjun0913@gmail.com>
2023-07-06 14:48:32 +0900

    feat: DB 연결 및 부트 클래스 추가

--- membership-service/src/main/kotlin/com/example/membership/MembershipApplication.kt
+++ membership-service/src/main/kotlin/com/example/membership/MembershipApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class MembershipApplication {
-    fun main(args: Array<String>) {
-        runApplication<MembershipApplication>(*args)
-    }
+class MembershipApplication
+
+fun main(args: Array<String>) {
+    runApplication<MembershipApplication>(*args)
 }
